d4428aa25d02ae2248cdb7e68f5fe86bad757865
Jimmy Mulek <jmulek218@gmail.com>
2016-03-08 00:43:07 -0600

    Add comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-resources :posts
-root "posts#index"
 
+  
+  resources :posts do
+  	resources :comments
+  end
 
- 
+  root "posts#index"
+
+  get '/about', to: 'pages#about'
+
 end
